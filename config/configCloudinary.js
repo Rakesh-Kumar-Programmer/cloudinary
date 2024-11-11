33d32396dc1eee57b707ade859ced0d3aeff44a3
Rakesh Kumar <rakesh.bahel@gmail.com>
2024-11-11 23:32:28 +0530

    File successfully upload to cloudinary and local server upload file deleted also

--- config/configCloudinary.js
+++ config/configCloudinary.js
@@ -3,43 +3,11 @@ import { api_Key } from './configServer.js';
 import { api_Secret } from './configServer.js';
 
 
-
-
-(async function () {
-    
-    cloudinary.config({ 
-    cloud_name: cloud_Name, 
-    api_key: api_Key, 
-    api_secret: api_Secret
-    });
+    // cloudinary.config({ 
+    // cloud_name: cloud_Name, 
+    // api_key: api_Key, 
+    // api_secret: api_Secret
+    // });
 
   
     
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(req.file.path)
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
-
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import { port } from "./config/configServer.js";
 import { upload } from "./config/configMulter.js";
 import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
 import { cloud_Name} from './config/configServer.js';
 import { api_Key } from './config/configServer.js';
 import { api_Secret } from './config/configServer.js';
@@ -10,27 +12,59 @@ cloudinary.config({
     cloud_name: cloud_Name, 
     api_key: api_Key, 
     api_secret: api_Secret
-    });
+});
 
 
 
 const app=express();
 
-app.post('/post',upload.single('myfile'),async (req, res)=>{
+// app.post('/post',upload.single('myfile'),async (req, res)=>{
         
+ 
+//     const x =await cloudinary.uploader.upload(req.file.path)
+//     console.log('file upload sucessfully to Coudinary : ', x );
+
+//         // Remove the file
+//     // fs.unlink((req.file.path),
+//     // function(err){ 
+//     // if (err) console.log(err); 
+//     // else console.log("\nDeleted file");
+//     // }) ;
+
+//     res.send('Hello World!')
+     
+// })
+app.post('/post',upload.single('myfile'),async (req,res)=>{
+    console.log(req.file.path)
+
     try {
-        const x =await cloudinary.uploader.upload(req.file.path)
-        console.log('file upload sucessfully to Coudinary : ', x );
+        const x= await cloudinary.uploader.upload(req.file.path)
+        console.log("cloudianry",x)
+
+        //const newvar = new Image({Image_Url:x.secure_url});
+        //newvar.save().then(() => console.log('kaam ho gaya'));
+        
+        // Delete upload local server file 
+        fs.unlink((req.file.path),
+        function(err)
+        { 
+            if (err) console.log(err); 
+            else console.log("\nDeleted local server file");
+        }) 
+
+        res.json({
+            msg:"file uploaded",
+            your_url:{image_url:x.secure_url}
+        })
     
-    res.send('Hello World!')
-    } catch (error) {
+    } catch (error) 
+    {
         console.log(error);
-        res.status(500).send('Error in uploading to cloudinary')
-                
+        res.status(500).json({msg: "Something went wrong"})
+      
     }
     
 })
-
 
 app.listen(port,()=>{
     console.log("server is running on " , port);
